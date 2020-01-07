76faa40852e74b17bbd7099cab7c3fab11e908ca
Adrien Croquelois <croquelois.adrien@gmail.com>
2020-01-07 23:57:13 +0100

    :green_heart:

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'https://desolate-citadel-61583.herokuapp.com/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
